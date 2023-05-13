c2cdef19bd658fed5b610915c579a90b9997b51e
Murilothebr <murilothebr@hotmail.com>
2023-05-12 22:14:18 -0300

    new folder structure

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GorpaGuideNode
 GorpaGuide com node
+
+##Como rodar:
+
+### Aplicação:  node server.js
+### Db : npx json-server --watch db.json
+
--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-  console.log('O servidor está rodando na porta localhost:3000');
+app.listen(4000, () => {
+  console.log('O servidor está rodando na porta localhost:4000');
 });
