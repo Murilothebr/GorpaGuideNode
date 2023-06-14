97de3cf4c83f203178db9f69ae84f5cba3174cc9
Murilothebr <murilothebr@hotmail.com>
2023-06-14 04:02:22 -0300

    fix: new personal frontend for index

--- public/obj/restaurante.js
+++ public/obj/restaurante.js
@@ -9,52 +9,10 @@ class Restaurante {
   }
 }
 
-class Restaurantes extends Restaurante {
-  restaurantes = [];
-  constructor() {
-    this.restaurantes = [];
-  }
-
-  adicionarRestaurante(restaurante) {
-    this.restaurantes.push(restaurante);
-  }
-
-  removerRestaurante(restauranteId) {
-    this.restaurantes.findIndex((id) => id == restauranteId).splice();
-  }
-
-  getRestauranteById(restauranteId) {
-    return this.restaurantes.findIndex((id) => id == restauranteId);
-  }
-
-  getRestaurantes() {
-    return this.restaurantes;
-  }
-}
-
 const restaurantes = [];
 
-function criarCardsDeRestaurantes() {
-  const container = document.querySelector(".restaurant-cards");
-  restaurantes.forEach((restaurante) => {
-    const card = `
-    <div class="card">
-    <h1 class="text-center h1">${restaurante.nome}</h1>
-    <div class="card-image">
-      <img src="${restaurante.imagem}" alt="Foto do ${restaurante.nome}">
-      <div class="status-circle open"></div>
-    </div>
-  </div>
-    `;
-
-    container.innerHTML += card;
-  });
-}
-
-
 function popularRestaurantes(json) {
-  console.log("populate");
-  var restaurantesJSON = json.restaurantes;
+  var restaurantesJSON = json;
   for (var i = 0; i < restaurantesJSON.length; i++) {
     restaurante = restaurantesJSON[i];
     restaurantes.push(new Restaurante(
@@ -66,6 +24,29 @@ function popularRestaurantes(json) {
       restaurante.horarioFuncionamento
     ));
   }
+}
+
+function criarCardsDeRestaurantes() {
+  
+  const container = document.querySelector(".restaurant-cards");
+
+		(setTimeout(() => {
+      restaurantes.forEach(
+        (restaurante) => {
+        console.log("1")
+        const card = `
+        <div class="card">
+        <h1 class="text-center h1">${restaurante.nome}</h1>
+        <div class="card-image">
+          <img src="${restaurante.imagem}" alt="Foto do ${restaurante.name}">
+          <div class="status-circle open"></div>
+        </div>
+      </div>
+        `;
+        container.innerHTML += card;
+      }
+      );
+		}, "0100"));
 }
 
 fetch('http://localhost:3000/restaurantes')
