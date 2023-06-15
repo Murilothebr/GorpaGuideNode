adfa8b8427d34852e4e25e848696303b96a0e1bb
Murilothebr <murilothebr@hotmail.com>
2023-06-14 21:40:34 -0600

    footer + jquery css selectors

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 app.use(express.static('public'));
 
 app.listen(4000, () => {
-  console.log('O servidor está rodando na porta localhost:4000');
+  console.log('O servidor está rodando na porta http://localhost:4000');
 });
